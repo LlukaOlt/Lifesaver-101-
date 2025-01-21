c622cf2b49f579dbeac9924a6240b16505ee09a2
sylejmani444 <osylejmani@york.citycollege.eu>
2025-01-21 17:55:20 +0200

    filefix

---
+++ javascript/jscript.js
@@ -0,0 +1,4 @@
+document.getElementById('bpm').addEventListener('click', function() {
+    const audio = new Audio('100bpmMetronome.mp3'); 
+    audio.play();
+});
--- jscript.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('bpm').addEventListener('click', function() {
-    const audio = new Audio('100bpmMetronome.mp3'); 
-    audio.play();
-});
