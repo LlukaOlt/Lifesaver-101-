8eb8e7745cd581602101b332336c320da14e1cb4
dingelidis <kdingelidis@york.citycollege.eu>
2025-01-21 20:16:19 +0200

    Update jscript.js

--- javascript/jscript.js
+++ javascript/jscript.js
@@ -1,3 +1,4 @@
+<!--Author: Konstantinos Dingelidis-->
 document.getElementById('bpm').addEventListener('click', function() {
     const audio = new Audio('100bpmMetronome.mp3'); 
     audio.play();
