9981b4b89c395c841223ab0d737e74a26fe6b08c
dingelidis <kdingelidis@york.citycollege.eu>
2025-01-21 18:38:04 +0200

    Update jscript.js

--- javascript/jscript.js
+++ javascript/jscript.js
@@ -2,3 +2,16 @@ document.getElementById('bpm').addEventListener('click', function() {
     const audio = new Audio('100bpmMetronome.mp3'); 
     audio.play();
 });
+
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("scrollToTop").style.display = "block";
+    } else {
+        document.getElementById("scrollToTop").style.display = "none";
+    }
+};
+
+document.getElementById("scrollToTop").onclick = function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
